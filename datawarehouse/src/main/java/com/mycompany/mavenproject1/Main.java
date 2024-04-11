bc17a2b0d55ab5c40ed1044ade9062814eb5cc2f
Ciro <lopezciromartin@gmail.com>
2024-04-11 14:22:43 -0300

    terminado
    dsada

--- datawarehouse/src/main/java/com/mycompany/mavenproject1/Main.java
+++ datawarehouse/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -18,11 +18,9 @@ import org.json.simple.parser.JSONParser;
 public class Main {
 
     public static void main(String[] args) {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
-
-        scheduler.scheduleAtFixedRate(() -> {
-            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/datawarehouse", "root", "root")) {
+        ScheduledExecutorService programar = Executors.newScheduledThreadPool(1);//crea un servicio de ejecución programada que ejecuta tareas de manera programada
+        programar.scheduleAtFixedRate(() -> {//con scheduleAtFixedRate programamos una tarea para ejecutarse periodicamente en un intervalo de tiempo fijo, lo hice con lambda
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/datawarehouse", "root", "root")) {//nos conectamos a la base de datos
 
                 if (!jsonYaCargadoHoy(connection)) {
 
@@ -33,11 +31,11 @@ public class Main {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        }, 0, 1, TimeUnit.MINUTES) ;
+        }, 0, 1, TimeUnit.MINUTES) ;// Indica la unidad de tiempo para el retraso inicial y el intervalo entre ejecuciones de la tarea, ademas del delay
 
 
         try {
-            scheduler.awaitTermination(3, TimeUnit.HOURS);
+            programar.awaitTermination(3, TimeUnit.HOURS);// bloquea la ejecución del hilo actual hasta que todas las tareas se hayan terminado
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -48,12 +46,13 @@ public class Main {
 
         LocalDate fechaActual = LocalDate.now();
 
-        String query = "SELECT COUNT(*) FROM dispo_alquiler_diaria WHERE fecha_dispo = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setObject(1, fechaActual);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    return resultSet.getInt(1) > 0;
+        String query = "SELECT COUNT(*) FROM dispo_alquiler_diaria WHERE fecha_dispo = ?";//cuenta el número de registros en la tabla dispo_alquiler_diaria que tienen la misma fecha de disponibilidad que la fecha actual
+        try (PreparedStatement statement = connection.prepareStatement(query)) {//evita ataques de inyección SQL
+            statement.setObject(1, fechaActual);// Se establece el valor del parámetro de la consulta sql con la fecha actual.
+            try (ResultSet resultSet = statement.executeQuery()) {//realizamos la consulta y obtenemos el resultado
+
+                if (resultSet.next()) {// Se mueve el cursor del conjunto de resultados al primer registro.
+                    return resultSet.getInt(1) > 0;//Se devuelve true si el valor de la primera columna del primer registro del conjunto de resultados es mayor que cero, lo que indica que se ha cargado al menos un JSON hoy
                 }
             }
         }
@@ -62,15 +61,15 @@ public class Main {
 
     private static void cargarJson(Connection connection) throws SQLException {
         LocalDate fechaActual = LocalDate.now();
-        LocalDateTime fechaDispo = LocalDateTime.of(fechaActual, LocalTime.now());
+        LocalDateTime fechaDispo = LocalDateTime.of(fechaActual, LocalTime.now());//ombina la fecha obtenida en el paso anterior con la hora actual para obtener un objeto LocalDateTime, que representa la fecha y hora actual. Esto se utilizará para establecer la fecha de disponibilidad en la base de datos.
 
-        JSONParser parser = new JSONParser();
+        JSONParser parser = new JSONParser();//Se crea un objeto JSONParser del paquete org.json.simple.parser para analizar el contenido del archivo JSON.
         try (FileReader reader = new FileReader("datos.JSON")) {
             Object obj = parser.parse(reader);
             JSONArray jsonArray = (JSONArray) obj;
             Set<String> nombresCampos = obtenerNombresCampos(connection); // Obtener los nombres de los campos de la tabla
 
-            String query = "INSERT INTO dispo_alquiler_diaria (id, rubro_nombre, estado, bienEmpresa_nombre, contratoEmpresa_nombre, propietario, contrato_estado, contrato_numeroComp, contrato_fechaFin, contrato_fecha, contrato_cliente_nombre, contrato_numero, contrato_fechaInicio, linea_nombre, propio, enTransito, ordenDeTrabajo_descripcion, ordenDeTrabajo_estado, ordenDeTrabajo_numeroComp, ordenDeTrabajo_fechaEntrega, ordenDeTrabajo_fechaInicio, sucursal_nombre, entregado, proveedor_nombre, articulo_codigo, bien_descripcion, bien_estado, bien_aFabricacion, bien_depositoAlmacen_nombre, bien_identificacion, bien_modelo, bien_serie, Fecha_dispo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            String query = "INSERT INTO dispo_alquiler_diaria (id, rubro_nombre, estado, bienEmpresa_nombre, contratoEmpresa_nombre, propietario, contrato_estado, contrato_numeroComp, contrato_fechaFin, contrato_fecha, contrato_cliente_nombre, contrato_numero, contrato_fechaInicio, linea_nombre, propio, enTransito, ordenDeTrabajo_descripcion, ordenDeTrabajo_estado, ordenDeTrabajo_numeroComp, ordenDeTrabajo_fechaEntrega, ordenDeTrabajo_fechaInicio, sucursal_nombre, entregado, proveedor_nombre, articulo_codigo, bien_descripcion, bien_estado, bien_aFabricacion, bien_depositoAlmacen_nombre, bien_identificacion, bien_modelo, bien_serie, Fecha_dispo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,  ?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             try (PreparedStatement statement = connection.prepareStatement(query)) {
                 for (Object item : jsonArray) {
                     JSONObject jsonObject = (JSONObject) item;
@@ -114,6 +113,13 @@ public class Main {
         }
         return nombresCampos;
     }
-
+    private static void eliminarRegistrosExistente(Connection connection) throws SQLException {
+        String query = "DELETE FROM dispo_alquiler_diaria WHERE fecha_dispo = ?";
+        LocalDate fechaActual = LocalDate.now();
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setObject(1, fechaActual);
+            statement.executeUpdate();
+        }
+    }
 
 }
